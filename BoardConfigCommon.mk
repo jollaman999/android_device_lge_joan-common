35685f2f95c3d39a401a7cc18420a8500038b7c2
jollaman999 <admin@jollaman999.com>
2018-10-30 20:02:54 +0900

    Initial support of TWRP [1/2]
    Decryption is not working now.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,6 +159,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 119267606528
 BOARD_FLASH_BLOCK_SIZE := 0x40000
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 TARGET_EXFAT_DRIVER := exfat
 
@@ -196,3 +197,32 @@ WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
 WIFI_DRIVER_STATE_OFF := 0
 WIFI_DRIVER_STATE_ON := 1
 PRODUCT_VENDOR_MOVE_ENABLED := true
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/soc/c900000.qcom\x2cmdss_mdp/c900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 158
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INCLUDE_NTFS_3G := true
+
+# We can use the factory reset button combo to enter recovery safely
+TW_IGNORE_MISC_WIPE_DATA := true
+
+# exFAT drivers included in the kernel
+TW_NO_EXFAT_FUSE := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+#TARGET_HW_DISK_ENCRYPTION := true
+#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+
+# Debug flags
+#TWRP_INCLUDE_LOGCAT := true
